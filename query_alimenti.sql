dab8167ebe7ccd98808787dfe3a25faa65addca7
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2026-03-18 13:57:51 +0100

    games

---
+++ query_alimenti.sql
@@ -0,0 +1,63 @@
+SELECT * FROM its2026_dai.alimenti;
+
+use its2026_dai;
+
+
+create table categorie_alimenti(
+	categoria_id int primary key auto_increment,
+    categoria_nome varchar(30)
+    );
+
+insert into categorie_alimenti (categoria_nome)  -- inserisci nel campo nome  
+select distinct  categoria  -- tutto i valori selezionati 
+from alimenti -- dalla tabella alimenti
+
+order by categoria -- ordinati alfa per categoria
+;
+
+select * from categorie_alimenti;
+
+alter table alimenti 
+add column categoria_id int;
+
+desc alimenti;
+
+set sql_safe_updates = 0;
+
+update alimenti a, categorie_alimenti cat
+set a.categoria_id = cat.categoria_id
+where a.categoria = cat.categoria_nome;
+
+alter table alimenti
+drop column categoria;
+
+select * from alimenti;
+
+select *
+
+from alimenti a  -- LEFT
+
+inner join  categorie_alimenti cat -- RIGHT
+
+on a.categoria_id = cat.categoria_id;
+
+drop table alimenti_grassi;
+create or replace view alimenti_grassi
+
+as 
+
+select 
+	a.prodotto,
+    cat.categoria_nome as 'Categoria Alimentare',
+    a.lipidi as grassi
+from alimenti as a
+join categorie_alimenti as cat using(categoria_id)
+
+where a.lipidi > ( select avg(lipidi) from alimenti )
+
+order by grassi desc;
+
+select * from alimenti_grassi where grassi > (select avg(grassi) from alimenti_grassi );
+
+
+
