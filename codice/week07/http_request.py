640b2d54272326f745b4da3132838dff96c137d8
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2026-03-27 11:32:59 +0100

    request 0.2

--- codice/week07/http_request.py
+++ codice/week07/http_request.py
@@ -1,12 +1,33 @@
 """Modulo per testare richieste"""
 
 import requests
+import sqlite3
 
 URL = 'https://raw.githubusercontent.com/maboglia/ProgrammingResources/refs/heads/master/tabelle/film/rotten-tomatoes-best-cartoons.json'
+
+db = sqlite3.connect('film.db')
+
+navetta = db.cursor()
+
+query = """
+CREATE TABLE IF NOT EXISTS film(
+cartoon_id INTEGER PRIMARY KEY AUTOINCREMENT,
+titolo TEXT NOT NULL,
+anno INTEGER DEFAULT 0
+);
+"""
+
+navetta.execute(query)
 
 risposta = requests.get(URL)
 
 cartoni = risposta.json()
 
 for cartone in cartoni:
-    print(f"INSERT into cartoni (titolo) VALUES ('{cartone['titolo']}');")
+    if cartone['titolo']:
+        navetta.execute("INSERT into film (titolo) VALUES (?);", (cartone['titolo'],))
+
+
+db.commit()
+
+db.close()
